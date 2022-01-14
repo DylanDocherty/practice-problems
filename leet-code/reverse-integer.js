2d172df31e2d843f010aa6a7f2bcb3ee7bf2ab89
DylanDocherty <dylandocherty98@gmail.com>
2022-01-14 11:19:44 -0500

    Added Solved to header

--- leet-code/reverse-integer.js
+++ leet-code/reverse-integer.js
@@ -1,29 +1,30 @@
 /**
  * Dylan Docherty
  * Find the question here https://leetcode.com/problems/reverse-integer/
+ * Solved
  */
 
 /**
  * @param {number} x
  * @return {number}
  */
- var reverse = function(x) {
-    
+var reverse = function (x) {
+
     let isNeg = false;
-    if(x < 0){
+    if (x < 0) {
         isNeg = true;
         x *= -1;
     }
-    
+
     let num = x.toString();//int to string
     let numReverse = "";
-    for(let i=num.length-1;i>=0;i--){    //for loop reversing the string
+    for (let i = num.length - 1; i >= 0; i--) {    //for loop reversing the string
         numReverse += num[i];
     }
 
-    numReverse = isNeg? -parseInt(numReverse) : parseInt(numReverse);
-    
-    if(numReverse >= (Math.pow(2,31) -1)  || numReverse <= -(Math.pow(2,31))){
+    numReverse = isNeg ? -parseInt(numReverse) : parseInt(numReverse);
+
+    if (numReverse >= (Math.pow(2, 31) - 1) || numReverse <= -(Math.pow(2, 31))) {
         return 0;
     }
     return numReverse;
